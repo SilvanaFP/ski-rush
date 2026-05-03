18671dce034f91ca14f47fc4488b7d86a05ff734
PereRR <pereroca845@gmail.com>
2026-05-03 16:33:27 +0200

    pere wipe2

---
+++ Test SkiRush/Assets/Scripts/Minigames/Wipe2/TreeHit.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TreeHit : MonoBehaviour
+{
+    public bool touched = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("TRIGGER con: " + other.name);
+
+        if (other.GetComponent<EdgeCollider2D>() != null)
+        {
+            touched = true;
+            Debug.Log("ÁRBOL TOCADO: " + gameObject.name);
+
+            // 🔥 DEBUG VISUAL
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
+    }
+}
